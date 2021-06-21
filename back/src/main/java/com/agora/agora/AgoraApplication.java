e7d019f40c1995680b40ae5e928b341b7ceadeea
manupedrozo <manupedr@gmail.com>
2021-06-21 17:47:31 -0300

    init

---
+++ back/src/main/java/com/agora/agora/AgoraApplication.java
@@ -0,0 +1,15 @@
+package com.agora.agora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class AgoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgoraApplication.class, args);
+	}
+
+}
---
+++ back/src/main/java/com/agora/agora/model/Identifiable.java
@@ -0,0 +1,5 @@
+package com.agora.agora.model;
+
+public interface Identifiable {
+    int getId();
+}
