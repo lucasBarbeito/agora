7779eb7a31ab9e4d2206753fb9ab57d4be0d38b3
Tomás Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2021-07-29 19:26:07 -0300

    Setup front

---
+++ front/src/App.js
@@ -0,0 +1,26 @@
+import {Box, Button, Container, CircularProgress, Grid} from '@material-ui/core';
+import './App.css';
+import {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <Container style={{display: "flex", flexDirection: "column"}}>
+        <h1>Agora</h1>
+        <Container style={{maxWidth: "600px"}}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Box style={{display: "flex", flexDirection: "column"}}>
+                <Button style={{alignSelf: "center"}} variant="contained" color="primary" onClick={() => alert("Hello!")}>
+                  Say hi
+                </Button>
+              </Box>
+            </Grid>
+          </Grid>
+        </Container>
+      </Container>
+    );
+  }
+}
+
+export default App;
