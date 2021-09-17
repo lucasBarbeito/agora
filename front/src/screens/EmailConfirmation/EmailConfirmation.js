3abf974487b0e6bc8a545e964a771a117dc6ced2
Catalina Mendizabal <68200289+CatalinaMendizabal@users.noreply.github.com>
2021-09-16 23:15:24 -0300

    08-07 Integración de lista de grupos y FIX: página de verificación de email (#55)
    * integracion lista grupos
    
    * Cambios en componentDidMount y requests con try catch

--- front/src/screens/EmailConfirmation/EmailConfirmation.js
+++ front/src/screens/EmailConfirmation/EmailConfirmation.js
@@ -1,73 +1,85 @@
-import React, { Component } from 'react'
-import {CircularProgress, Grid,Button} from '@material-ui/core';
+import React, {Component} from 'react'
+import {CircularProgress, Grid, Button} from '@material-ui/core';
 import "./EmailConfirmation.css";
 
 
 export default class EmailConfirmation extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      requestSuccess: false,
+    constructor(props) {
+        super(props);
+        this.state = {
+            requestSuccess: false,
+            message: ''
+        }
     }
-  }
 
-  makeRequest = async (id) => {
-    const response = await fetch('http://localhost:8080/user/verify_user', {
-                        method: 'POST',
-                        body: JSON.stringify({
-                          userVerificationToken: this.props.match.params.id,
-                        }),
-                        headers: {
-                          'Content-type': 'application/json; charset=UTF-8',
-                        },
-                    });
-  }
+    makeRequest = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/user/verify_user', {
+                method: 'POST',
+                body: JSON.stringify({
+                    userVerificationToken: this.props.match.params.id,
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            });
+            if (response.ok) {
+                this.setState({
+                    message: 'Tu correo ha sido confirmado exitosamente',
+                    requestSuccess: true
+                });
+            } else {
+                console.log("respuesta " + response.status)
+                this.setState({
+                    message: 'Ha ocurrido un error!',
+                    requestSuccess: true
+                });
+            }
+        } catch (e) {
+            alert('Error, no es posible conectarse al back-end');
+        }
 
-  componentDidMount() {
-    try {
-      this.makeRequest();
-      this.setState({requestSuccess: true});
-      setTimeout(() => { 
-        this.props.history.push('/');
-      }, 2500);
-    } catch {
-      this.props.history.push('/');
     }
-  }
-    
-  render() {
-    return (
-      <div className="Agorabackground">
-          { this.state.requestSuccess ?
-            <Grid container id='email-verification-main-grid'>
-            <Grid item xs={6}>
-                <Grid container id="email-verification-left-grid">
-                    <Grid item xs={12}>
-                        <h1 className='email-verification-description-text'>Tu correo ha sido confirmado exitosamente</h1>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Grid container spacing={3} id="email-verification-buttons-grid">
-                            <Grid item xs={3}>
-                                <Button id="email-verification-button" variant="contained" color="primary" onClick = {() => this.props.history.push("/")}> Volver al inicio</Button>
+
+    componentDidMount() {
+        this.makeRequest();
+    }
+
+    render() {
+        return (
+            <div className="Agorabackground">
+                {this.state.requestSuccess ?
+                    <Grid container id='email-verification-main-grid'>
+                        <Grid item xs={6}>
+                            <Grid container id="email-verification-left-grid">
+                                <Grid item xs={12}>
+                                    <h1 className='email-verification-description-text'>{this.state.message}</h1>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Grid container spacing={3} id="email-verification-buttons-grid">
+                                        <Grid item xs={3}>
+                                            <Button id="email-verification-button" variant="contained" color="primary"
+                                                    onClick={() => this.props.history.push("/")}> Volver al
+                                                inicio</Button>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
                             </Grid>
                         </Grid>
+                        <Grid item xs={6}>
+                            <img className="email-verification-agora-Logo" alt="AgoraLogo" src='/agora-logo.png'/>
+                        </Grid>
                     </Grid>
-                 </Grid>
-             </Grid>
-            <Grid item xs={6}>
-                <img className="email-verification-agora-Logo" alt ="AgoraLogo" src='/agora-logo.png' />
-            </Grid>
-           </Grid>
-            :
-            <div id="email-verification-spinner-container">
-              <CircularProgress id="email-verification-spinner" size={40} />
+                    :
+                    <div id="email-verification-spinner-container">
+                        <CircularProgress id="email-verification-spinner" size={40}/>
+                    </div>
+                }
             </div>
-          }
-      </div>
-    )
-  }
-} 
+        )
+    }
+}
 
 
 
