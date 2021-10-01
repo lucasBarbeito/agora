28e9b45b92af9e183a82c7122cebe94a96c66a3d
agustinVon <agustin.von@ing.austral.edu.ar>
2021-10-01 20:38:25 -0300

    Modified initDB

--- back/src/main/java/com/agora/agora/StartUpDatabase.java
+++ back/src/main/java/com/agora/agora/StartUpDatabase.java
@@ -3,10 +3,14 @@ package com.agora.agora;
 import com.agora.agora.model.StudyGroup;
 import com.agora.agora.model.StudyGroupUser;
 import com.agora.agora.model.User;
+import com.agora.agora.model.form.StudyGroupForm;
+import com.agora.agora.model.form.UserForm;
 import com.agora.agora.model.type.UserType;
 import com.agora.agora.repository.StudyGroupRepository;
 import com.agora.agora.repository.StudyGroupUsersRepository;
 import com.agora.agora.repository.UserRepository;
+import com.agora.agora.service.StudyGroupService;
+import com.agora.agora.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
@@ -14,18 +18,25 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 @Component
 public class StartUpDatabase {
     private UserRepository userRepository;
+    private UserService userService;
+    private StudyGroupService studyGroupService;
     private StudyGroupRepository studyGroupRepository;
     private StudyGroupUsersRepository studyGroupUsersRepository;
 
     @Autowired
-    public StartUpDatabase(UserRepository userRepository, StudyGroupRepository studyGroupRepository, StudyGroupUsersRepository studyGroupUsersRepository) {
+    public StartUpDatabase(UserRepository userRepository, StudyGroupRepository studyGroupRepository, StudyGroupUsersRepository studyGroupUsersRepository, UserService userService, StudyGroupService studyGroupService) {
         this.userRepository = userRepository;
         this.studyGroupRepository = studyGroupRepository;
         this.studyGroupUsersRepository = studyGroupUsersRepository;
+        this.userService = userService;
+        this.studyGroupService = studyGroupService;
     }
 
     @EventListener
@@ -35,37 +46,89 @@ public class StartUpDatabase {
         if(init){
             try{
                 //Users
-                User user1 = new User("Pepe", "Perez", "perez@gmail.com", BCrypt.hashpw("Perez2020", BCrypt.gensalt()), true, UserType.USER);
-                User user2 = new User("Tomas", "Gomez", "gomez@gmail.com", BCrypt.hashpw("Gomez2019", BCrypt.gensalt()), false, UserType.USER);
-                User user3 = new User("Agustin", "Rodriguez", "rodriguez@gmail.com", BCrypt.hashpw("Rodriguez2018", BCrypt.gensalt()), true, UserType.USER);
-                User user4 = new User("Delfina", "Alonso", "alonso@gmail.com", BCrypt.hashpw("Alonso2017", BCrypt.gensalt()), true, UserType.USER);
-                User user5 = new User("Agustina", "Lopez", "lopez@gmail.com", BCrypt.hashpw("Lopez2016", BCrypt.gensalt()), false, UserType.USER);
-                User user6 = new User("Romina", "Diaz", "diaz@gmail.com", BCrypt.hashpw("Diaz2015", BCrypt.gensalt()), false, UserType.USER);
+                UserForm user1 = new UserForm("Pepe", "Perez", "perez@gmail.com", "Perez2020");
+                UserForm user2 = new UserForm("Tomas", "Gomez", "gomez@gmail.com", "Gomez2019");
+                UserForm user3 = new UserForm("Agustin", "Rodriguez", "rodriguez@gmail.com","Rodriguez2018");
+                UserForm user4 = new UserForm("Delfina", "Alonso", "alonso@gmail.com","Alonso2017");
+                UserForm user5 = new UserForm("Agustina", "Lopez", "lopez@gmail.com","Lopez2016");
+                UserForm user6 = new UserForm("Romina", "Diaz", "diaz@gmail.com", "Diaz2015");
+                UserForm user7 = new UserForm("Matias", "Lopez", "matias@gmail.com","Matias2020");
+                UserForm user8 = new UserForm("Carlos", "Mendez", "carlos@gmail.com", "Carlos2019");
+                UserForm user9 = new UserForm("Sofia", "Gimenez", "sofia@gmail.com", "Sofia2018");
+                UserForm user10 = new UserForm("Micaela", "Diaz", "alonso@gmail.com", "Micaela2017");
+                UserForm user11 = new UserForm("Fernando", "Rojas", "lopez@gmail.com","Fernando2016");
+                UserForm user12 = new UserForm("Juan", "Cruz", "diaz@gmail.com", "Juan2015");
 
-                userRepository.save(user1);
-                userRepository.save(user2);
-                userRepository.save(user3);
-                userRepository.save(user4);
-                userRepository.save(user5);
-                userRepository.save(user6);
+                ArrayList<Integer> userIds = new ArrayList<>();
+                userIds.add(userService.save(user1));
+                userIds.add(userService.save(user2));
+                userIds.add(userService.save(user3));
+                userIds.add(userService.save(user4));
+                userIds.add(userService.save(user5));
+                userIds.add(userService.save(user6));
+                userIds.add(userService.save(user7));
+                userIds.add(userService.save(user8));
+                userIds.add(userService.save(user9));
+                userIds.add(userService.save(user10));
+                userIds.add(userService.save(user11));
+                userIds.add(userService.save(user12));
 
                 //Study Groups
-                StudyGroup group1 = new StudyGroup("Teologia", "Estudios de Dios", user1, LocalDate.of(2016, 7, 1));
-                StudyGroup group2 = new StudyGroup("Platon", "Estudios de Platon", user3, LocalDate.of(2020, 10, 25));
-                StudyGroup group3 = new StudyGroup("Aristoteles", "Estudios de Aristoteles", user4, LocalDate.of(2019, 3, 6));
+                StudyGroupForm group1 = new StudyGroupForm("Teologia", "Estudios de Dios",userIds.get(0),LocalDate.of(2020, 10, 25));
+                StudyGroupForm group2 = new StudyGroupForm("Platon", "Estudios de Platon", userIds.get(2), LocalDate.of(2020, 10, 25));
+                StudyGroupForm group3 = new StudyGroupForm("Aristoteles", "Estudios de Aristoteles", userIds.get(4), LocalDate.of(2019, 3, 6));
+                StudyGroupForm group4 = new StudyGroupForm("Analisis matematico", "Estudios de matematicas", userIds.get(6), LocalDate.of(2016, 7, 1));
+                StudyGroupForm group5 = new StudyGroupForm("Geometria", "Estudios de las formas", userIds.get(8), LocalDate.of(2020, 10, 25));
+                StudyGroupForm group6 = new StudyGroupForm("Geografia", "Estudios de la tierra", userIds.get(10), LocalDate.of(2019, 3, 6));
+                StudyGroupForm group7 = new StudyGroupForm("Historia romana", "Estudios de la historia romana", userIds.get(1), LocalDate.of(2016, 7, 1));
+                StudyGroupForm group8 = new StudyGroupForm("Legales", "Estudios de leyes", userIds.get(3), LocalDate.of(2020, 10, 25));
+                StudyGroupForm group9 = new StudyGroupForm("Composicion musical", "Estudios de la musica", userIds.get(5), LocalDate.of(2019, 3, 6));
+                StudyGroupForm group10 = new StudyGroupForm("Jardineria", "Estudios del cuidado de las plantas", userIds.get(7), LocalDate.of(2016, 7, 1));
+                StudyGroupForm group11 = new StudyGroupForm("Biologia", "Estudios de los seres vivos", userIds.get(9), LocalDate.of(2020, 10, 25));
+                StudyGroupForm group12 = new StudyGroupForm("Cartografia", "Estudios de los mapas", userIds.get(11), LocalDate.of(2019, 3, 6));
+                StudyGroupForm group13 = new StudyGroupForm("Lenguajes", "Estudios de lenguajes de programacion", userIds.get(0), LocalDate.of(2016, 7, 1));
+                StudyGroupForm group14 = new StudyGroupForm("Analisis sintactico", "Estudio y analisis de la composicion del lenguaje", userIds.get(2), LocalDate.of(2020, 10, 25));
+                StudyGroupForm group15 = new StudyGroupForm("Deportes", "Estudios de los deportes", userIds.get(4), LocalDate.of(2019, 3, 6));
+                StudyGroupForm group16 = new StudyGroupForm("Diseño grafico", "Estudios de diseño de imagen y sonido", userIds.get(6), LocalDate.of(2016, 7, 1));
+                StudyGroupForm group17 = new StudyGroupForm("Corte y confeccion", "Estudios de las telas", userIds.get(8), LocalDate.of(2020, 10, 25));
+                StudyGroupForm group18 = new StudyGroupForm("Teatro", "Estudios de obras teatrales", userIds.get(10), LocalDate.of(2019, 3, 6));
+                StudyGroupForm group19 = new StudyGroupForm("Algebra", "Estudios de algebra tradicional", userIds.get(1), LocalDate.of(2016, 7, 1));
+                StudyGroupForm group20 = new StudyGroupForm("IntroCom", "Estudios de introduccion a la computacion", userIds.get(3), LocalDate.of(2020, 10, 25));
+                StudyGroupForm group21 = new StudyGroupForm("AYED", "Estudio de algoritmos y estructura de datos", userIds.get(5), LocalDate.of(2019, 3, 6));
 
-                studyGroupRepository.save(group1);
-                studyGroupRepository.save(group2);
-                studyGroupRepository.save(group3);
+                ArrayList<Integer> groupIds = new ArrayList<>();
+                groupIds.add(studyGroupService.create(group1));
+                groupIds.add(studyGroupService.create(group2));
+                groupIds.add(studyGroupService.create(group3));
+                groupIds.add(studyGroupService.create(group4));
+                groupIds.add(studyGroupService.create(group5));
+                groupIds.add(studyGroupService.create(group6));
+                groupIds.add(studyGroupService.create(group7));
+                groupIds.add(studyGroupService.create(group8));
+                groupIds.add(studyGroupService.create(group9));
+                groupIds.add(studyGroupService.create(group10));
+                groupIds.add(studyGroupService.create(group11));
+                groupIds.add(studyGroupService.create(group12));
+                groupIds.add(studyGroupService.create(group13));
+                groupIds.add(studyGroupService.create(group14));
+                groupIds.add(studyGroupService.create(group15));
+                groupIds.add(studyGroupService.create(group16));
+                groupIds.add(studyGroupService.create(group17));
+                groupIds.add(studyGroupService.create(group18));
+                groupIds.add(studyGroupService.create(group19));
+                groupIds.add(studyGroupService.create(group20));
+                groupIds.add(studyGroupService.create(group21));
 
-                //StudyGroups with creator User relations (to appear in users list in studyGroup)
-                StudyGroupUser group1User1 = new StudyGroupUser(user1, group1);
-                StudyGroupUser group2User3 = new StudyGroupUser(user3, group2);
-                StudyGroupUser group3User4 = new StudyGroupUser(user4, group3);
-
-                studyGroupUsersRepository.save(group1User1);
-                studyGroupUsersRepository.save(group2User3);
-                studyGroupUsersRepository.save(group3User4);
+                for (Integer groupId: groupIds) {
+                    ArrayList<Integer> list = new ArrayList<>();
+                    for (int i = 0; i < 11; i++) {
+                        list.add(i);
+                    }
+                    Collections.shuffle(list);
+                    for (int i = 0; i < 4; i++) {
+                        studyGroupService.addUserToStudyGroup(groupId,userIds.get(list.get(i)));
+                    }
+                }
 
             } catch (Exception ignored){}
         }
