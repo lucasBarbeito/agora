7c25f30c753e4fec32d44d57436af3a985b63e9b
NicolasWerner97 <nicolas.werner@ing.austral.edu.ar>
2021-10-25 11:44:12 -0300

    Added multiple contact Links to StartUpDatabase.

---
+++ back/src/main/java/com/agora/agora/repository/ContactLinkRepository.java
@@ -0,0 +1,8 @@
+package com.agora.agora.repository;
+
+import com.agora.agora.model.ContactLink;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContactLinkRepository extends CrudRepository<ContactLink, Integer> {
+
+}
